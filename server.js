435f7dedc86f9d7caba9300baa17e2aeb41a7065
Ishan Polusta <polustaishan2001@gmail.com>
2021-07-07 20:57:18 +0530

    server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+
 const app = express();
+
 const http = require("http").createServer(app);
+
 const PORT = process.env.PORT || 3000;
-const users = {};
 
 http.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
@@ -17,21 +19,9 @@ app.get("/", (req, res) => {
 const io = require("socket.io")(http);
 
 io.on("connection", (socket) => {
-  socket.on("new-user-joined", (name) => {
-    console.log("New user", name);
-    users[socket.id] = name;
-    socket.broadcast.emit("user-joined", name);
-  });
+  console.log("Connected...");
 
-  socket.on("send", (message) => {
-    socket.broadcast.emit("receive", {
-      message: message,
-      name: users[socket.id],
-    });
-  });
-
-  socket.on("disconnect", (message) => {
-    socket.broadcast.emit("left", users[socket.id]);
-    delete users[socket.id];
+  socket.on("message", (msg) => {
+    socket.broadcast.emit("message", msg);
   });
 });
